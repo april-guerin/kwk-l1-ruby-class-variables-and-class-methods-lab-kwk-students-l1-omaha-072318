a0d18805f48130256515699289af61577530234a
april-guerin <aprilguerin@yahoo.com>
2018-07-30 14:47:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,22 @@
 class Song
   @@count = 0
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres <
   end
   attr_accessor :name, :artist, :genre
   
   def self.count
     @@count
   end
+  
+  def self.genres
+    @@genres
+  end
 end
+
+
